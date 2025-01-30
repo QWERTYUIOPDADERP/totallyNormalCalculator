92fa52dd480e577f06746c16020cb59857a69f5c
Matthew <73604168+QWERTYUIOPDADERP@users.noreply.github.com>
2025-01-30 13:24:34 -0800

    zero was parsing as false so I added a conditional for that

--- app.js
+++ app.js
@@ -75,13 +75,13 @@ function setCalculatorScreen(value) {
 
 //Function to add to current number (ie: 3 to 32 after 2 was clicked)
 function numClick(num) {
-    if (!num1) { //If the first number doesn't exist, set it
+    if (!(num1)&&!(num1==0)) { //If the first number doesn't exist, set it
         num1 = num;
         setCalculatorScreen(num1);
     } else if (!operator && !num2 && ((num1 + '').length < 6)) { //If the first number does exist and isn't too long, add a number to it
         num1 = (num1 + "" + num);
         setCalculatorScreen(num1);
-    } else if (num1 && operator && !num2) { //If the second number doesn't exist, set it
+    } else if (num1 && operator && !num2 && !(num2==0)) { //If the second number doesn't exist, set it
         num2 = num;
         setCalculatorScreen(num2);
     } else if (num1 && operator && ((num2 + '').length < 6)) { //if the second number does exist and isn't too long, add to it
@@ -91,7 +91,7 @@ function numClick(num) {
 }
 
 function decimal() { //Adds a decimal point to the current number
-    if (!(num1)) { //If the first number doesn't exist, set to '0.1'
+    if (!(num1+'')) { //If the first number doesn't exist, set to '0.1'
         numClick('0.');
     } else if (!operator && !(num2)) { //If the first number does exist and is still in focus
         if (!((num1 + '').includes('.'))) //If the number doesn't already have a decimal point
@@ -105,14 +105,14 @@ function decimal() { //Adds a decimal point to the current number
 }
 
 function opClick(op) { //Sets operator
-    if (num1 && !num2) { //If the first number has been selected and the second hasn't
+    if ((num1+'') && !num2) { //If the first number has been selected and the second hasn't
         operator = op;
         setCalculatorScreen(operator);
     }
 }
 
 function enter() { //If the enter button is clicked
-    if (num1 && operator && num2) { //If there is a first number, an operator, and a second number
+    if ((num1+'') && operator && (num2+'')) { //If there is a first number, an operator, and a second number
         doMath();
     }
 }
